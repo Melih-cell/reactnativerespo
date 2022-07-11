5a7294fa76db2fe0b82f3b16fd08aa18a616df0d
Melih Dokuzlar <melihdokuzlar71@gmail.com>
2022-07-11 18:53:46 +0300

    first

---
+++ awesome/App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{justifyContent: 'center'}}>
+      <Text style={{fontSize: 50, alignSelf: 'center', color: 'red', justifyContent: 'center'}}>İt Oğlu İt</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
